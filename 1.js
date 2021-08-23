cf6de27e548533c7ef77b9817d95b11d37a2f77c
galina <galinakalashnikova197743@gmail.com>
2021-08-23 08:51:47 +0700

    first commit

---
+++ 1.js
@@ -0,0 +1,12 @@
+const input = document.getElementById("scope")
+const clear = document.getElementById("clear")
+//на кнопку clear добавляется событие "нажатие мыши"
+//на кнопку clear добавляется событие "нажатие мыши"
+clear.addEventListener(
+    "click",
+    //когда нажмем, то выполнится функция
+    function (event) { 
+        //содержимое текстового поля выводится в консоль       
+        console.log(input.value)
+    }  
+)
